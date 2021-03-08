6538a7fe100abb47ad57e6e06a5067acca0d12cd
EisukeTochitani <kop.michael1958@gmail.com>
2021-03-08 11:50:03 +0000

    [finish]task5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
- devise_for :users
- root 'homes#top'
+  get 'search/search'
+  devise_for :users
+  root 'homes#top'
   get 'home/about' 
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
   resource :favorites,only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
   end
+  
+  get '/search', to: 'search#search'
+  
   resources :users,only: [:show,:index,:edit,:update] do
     member do
       get :following, :followers
